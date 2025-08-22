eb20f49439eedd810f33a3d95827d0f13d920f1d
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2025-08-21 22:48:31 -0300

    Add files via upload

--- TimeSeries.R
+++ TimeSeries.R
@@ -3,3 +3,11 @@ library(TSA)
 
 data(JJ)
 plot(JJ, type="o", ylab="Quarterly Earnings per Share")
+
+## MOVING AVAREGE
+w <- rnorm(500, mean = 0, sd = 1)
+v <- filter(w, sides = 2, rep(1/3, 3))
+par(mfrow = c(2, 1), mar = c(4, 4, 2, 1))  # bottom, left, top, right
+plot.ts(w, main = "White Noise")
+plot.ts(v, main = "Moving Average")
+
